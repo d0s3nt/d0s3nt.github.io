08a5092bb1164967a4cf995cbc5a1a9140f7dedf
c0114tz <d0s3nt@proton.me>
2024-12-28 16:54:06 +0100

    fix elixir

--- _site/assets/js/data/swconf.js
+++
@@ -1,1 +0,0 @@
-const swconf = { cacheName: 'chirpy-1735375916',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/hello/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,30 @@
-const swconf = { cacheName: 'chirpy-1735375916',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/hello/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = {
+  
+    cacheName: 'chirpy-1735401040',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/hello/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
